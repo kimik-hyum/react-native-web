f51f74bc2e7a0c6c9188624704bf67fb6f474ade
hyunjun-kim89 <hyunjun.k@braincommerce.com>
2021-02-07 15:11:57 +0900

    react native web 초기 세팅

---
+++ App.web.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Hello world from react</Text>
+    </View>
+  )
+}
+
+export default App
