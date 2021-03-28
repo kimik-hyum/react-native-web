a07d8c992a66a7b139eab06c618fecd5425314b2
hyunjun-kim89 <hyunjun.k@braincommerce.com>
2021-03-28 17:39:35 +0900

    react native 세팅 변경 및 react 버전 변경

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
-const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
   template: path.resolve(__dirname, './public/index.html'),
   filename: 'index.html',
   inject: 'body',
-})
+});
 
 module.exports = {
   entry: path.join(__dirname, 'index.web.js'),
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, '/build'),
+    environment: {
+      arrowFunction: false,
+      bigIntLiteral: false,
+      const: false,
+      destructuring: false,
+      dynamicImport: false,
+      forOf: false,
+      module: false,
+    },
   },
   resolve: {
     alias: {
@@ -34,9 +43,10 @@ module.exports = {
   },
   plugins: [HTMLWebpackPluginConfig],
   devServer: {
+    host: '0.0.0.0',
     open: true,
     historyApiFallback: true,
     contentBase: './',
     hot: true,
   },
-}
+};
